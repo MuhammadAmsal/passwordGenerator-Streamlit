3f67c121b4e078655d34940f25de704f1d616ad3
Muhammad Amsal <muhammadamsal520@gmail.com>
2025-03-10 00:11:16 +0500

    Update passwordGenerator.py

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -3,6 +3,8 @@ import random
 import string
 # import pyperclip
 
+generated_passwords = set()
+
 def generate_password(length, use_numbers, use_characters, use_alphabets):
     characters = ""
     if use_numbers:
@@ -15,21 +17,40 @@ def generate_password(length, use_numbers, use_characters, use_alphabets):
     if not characters:
         return "Please select at least one option"
     
-    password = ''.join(random.choice(characters) for _ in range(length))
-    return password
+    while True:
+        password = ''.join(random.choice(characters) for _ in range(length))
+        if password not in generated_passwords:
+            generated_passwords.add(password)
+            return password
 
 def check_password_strength(password):
-    length = len(password)
-    has_digit = any(char.isdigit() for char in password)
-    has_letter = any(char.isalpha() for char in password)
-    has_special = any(char in string.punctuation for char in password)
+    score = 0
+    if len(password) >= 8:
+        score += 1
+    if any(char.islower() for char in password) and any(char.isupper() for char in password):
+        score += 1
+    if any(char.isdigit() for char in password):
+        score += 1
+    if any(char in "!@#$%^&*" for char in password):
+        score += 1
+    if len(password) >= 12:
+        score += 1
     
-    if length >= 10 and has_digit and has_letter and has_special:
+    if score == 5:
         return "Strong"
-    elif length >= 6 and has_digit and has_letter:
-        return "Good"
+    elif 3 <= score <= 4:
+        return "Moderate"
     else:
         return "Weak"
+
+def provide_feedback(password):
+    strength = check_password_strength(password)
+    if strength == "Weak":
+        return "Try making your password at least 8 characters long, including uppercase letters, numbers, and special characters."
+    elif strength == "Moderate":
+        return "Good password! Consider adding more length or special characters for extra security."
+    else:
+        return "Your password is strong! Well done."
 
 st.title("Password Generator")
 
@@ -42,13 +63,17 @@ generate = st.button("Generate Password")
 
 if "password" not in st.session_state:
     st.session_state.password = ""
+    st.session_state.previous_passwords = set()
 
 if generate:
-    st.session_state.password = generate_password(length, use_numbers, use_characters, use_alphabets)
+    new_password = generate_password(length, use_numbers, use_characters, use_alphabets)
+    st.session_state.password = new_password
+    st.session_state.previous_passwords.add(new_password)
 
 password = st.text_input("Generated Password", st.session_state.password)
 password_strength = check_password_strength(password)
 st.write(f"Password Strength: **{password_strength}**")
+st.write(provide_feedback(password))
 
 # if st.button("Copy to Clipboard"):
  #   pyperclip.copy(password)
